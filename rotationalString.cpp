75098cfff47abe1a876322f6315d33233c059826
Parth-Bagwe <parthbagwee@gmail.com>
2026-07-17 21:56:17 +0530

    add rotational strings

--- rotationalString.cpp
+++ rotationalString.cpp
@@ -1,15 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Solution {
 public:
-    // Check if 'goal' is a rotation of 's'
     bool rotateString(string& s, string& goal) {
         // Strings must be same length to be rotations of each other
         if (s.length() != goal.length()) {
             return false;
         }
-        // Try all possible rotations of 's'
         for (int i = 0; i < s.length(); i++) {
             string rotated = s.substr(i) + s.substr(0, i);  
             if (rotated == goal) {
@@ -19,18 +16,13 @@ public:
         return false;
     }
 };
-
 int main() {
     Solution sol;
     string s = "rotation";
     string goal = "tionrota";
-    
-    // Output the result
     if (sol.rotateString(s, goal)) {
         cout << "true" << endl;  
     } else {
         cout << "false" << endl;
     }
-
     return 0;
-}
