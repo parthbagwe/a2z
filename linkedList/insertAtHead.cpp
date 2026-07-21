6b643a87feb5c9c9db7918d4293cfa141d556451
Parth-Bagwe <parthbagwee@gmail.com>
2026-07-21 22:35:14 +0530

    file changes

---
+++ linkedList/insertAtHead.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node(int data1, Node* next1){
+            data=data1;
+            next=next1;
+        }
+};
+int main(){
+    
+}
